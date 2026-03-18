a151ef1bfa4277fd6d34ac1415314904025ba2e6
Furkan <furkankokcu46@gmail.com>
2026-03-19 00:16:51 +0300

    Update service worker cache version and files

--- sw.js
+++ sw.js
@@ -1,23 +1,25 @@
-const CACHE_ADI = "hesy-tools-v1"; // Versiyon güncelleyince burayı v2 yaparsın
+const CACHE_ADI = "hesy-tools-v2"; // Versiyon güncelleyince burayı v2 yaparsın
 const DOSYALAR = [
   "./",
-  "./index.html",
-  "./main.js",
-  "./receteler.json",
-  "./pedoguide.json",
-  "./lathas.json",       // Eğer json kullanıyorsan
-  "./herbst.json",       // Eğer json kullanıyorsan
-  "./round-icon-new.png", // Logon
-  "./who.png",
-  "./mainlogo-Photoroom.png",
-  "./manifest.json",
-  "https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css",
-  "https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js",
-  "https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js",
-  "https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js",
-  "https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js",
-  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" 
-  // NOT: Font Awesome kullanıyorsan linkini buraya tam ekle
+"./index.html",
+"./main.js",
+"./receteler.json",
+"./pedoguide.json",
+"./lathas.json",       // Eğer json kullanıyorsan
+"./herbst.json",       // Eğer json kullanıyorsan
+"./round-icon-new.png", // Logon
+"./who.png",
+"./mainlogo-Photoroom.png",
+"./manifest.json",
+"./materia.css",
+"./cyborg.css",
+"https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css",
+"https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js",
+"https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js",
+"https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js",
+"https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js",
+"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+// NOT: Font Awesome kullanıyorsan linkini buraya tam ekle
 ];
 
 // 1. KURULUM (Dosyaları Önbelleğe Al)
@@ -52,8 +54,8 @@ self.addEventListener("fetch", (olay) => {
     caches.match(olay.request).then((cevap) => {
       // Cache'de varsa onu döndür, yoksa internetten çek
       return cevap || fetch(olay.request).catch(() => {
-          // Eğer internet de yoksa ve dosya cache'de yoksa (opsiyonel hata sayfası)
-          // return caches.match("./offline.html"); 
+        // Eğer internet de yoksa ve dosya cache'de yoksa (opsiyonel hata sayfası)
+        // return caches.match("./offline.html");
       });
     })
   );
