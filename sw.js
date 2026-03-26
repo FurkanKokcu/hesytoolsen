1351965e932e0251675a1519ae0c21eb1d147a1d
Furkan <furkankokcu46@gmail.com>
2026-03-27 00:08:19 +0300

    Update service worker file with new assets

--- sw.js
+++ sw.js
@@ -11,10 +11,11 @@ const DOSYALAR = [
 "./round-icon-new.png",
 "./round-icon.png",
 "./who.png",
+"./williams.png",
 "./mainlogo-Photoroom.png",
 "./manifest.json",
-"./materia.css",
-"./cyborg.css",
+"./flatly.css",
+"./darkly.css",
 // Kullanılan CDN linkleri (Çevrimdışı çalışabilmesi için)
 "https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.min.js",
 "https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js",
